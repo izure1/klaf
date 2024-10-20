32a7d80f74c97ed3fd51806b8b356e8fb77314b4
izure <izure@naver.com>
2024-10-20 20:23:44 +0900

    perf: improve cache handling to allow garbage collection of cached data during low memory conditions

--- src/utils/TextConverter.ts
+++ src/utils/TextConverter.ts
@@ -1,25 +1,20 @@
+import { CacheEntanglementSync } from 'cache-entanglement'
+
 export class TextConverter {
   protected static readonly Encoder = new TextEncoder()
   protected static readonly Decoder = new TextDecoder()
-  private static readonly _CachedText = new Map<string, string>()
-  private static readonly _CachedRaw = new Map<string, number[]>()
+  private static readonly _CachedText = new CacheEntanglementSync((key, state, array: number[]) => {
+    return TextConverter.Decoder.decode(Uint8Array.from(array))
+  })
+  private static readonly _CachedRaw = new CacheEntanglementSync((key, state) => {
+    return Array.from(TextConverter.Encoder.encode(key))
+  })
 
   static FromArray(array: number[]): string {
-    const key = array.join(',')
-    if (!TextConverter._CachedText.has(key)) {
-      const data = TextConverter.Decoder.decode(Uint8Array.from(array))
-      TextConverter._CachedText.set(key, data)
-    }
-    return TextConverter._CachedText.get(key)!
+    return TextConverter._CachedText.cache(array.join(','), array).raw
   }
 
   static ToArray(str: string): number[] {
-    if (!TextConverter._CachedRaw.has(str)) {
-      const data = Array.from(TextConverter.Encoder.encode(str))
-      TextConverter._CachedRaw.set(str, data)
-    }
-    const data = TextConverter._CachedRaw.get(str)!
-    const clone = data.slice()
-    return clone
+    return TextConverter._CachedRaw.cache(str).clone('array-shallow-copy')
   }
 }
