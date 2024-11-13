05b1f0aa82b1451df778b9c6e03659ba970fdb91
izure <izure@naver.com>
2024-11-14 01:32:39 +0900

    chore: update dependency modules

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,6 @@
 const { TextDecoder: NodeTextDecoder, TextEncoder: NodeTextEncoder } = require('util')
+const _structuredClone = require('core-js/actual/structured-clone')
 
 global.TextDecoder = NodeTextDecoder
 global.TextEncoder = NodeTextEncoder
+global.structuredClone = _structuredClone
